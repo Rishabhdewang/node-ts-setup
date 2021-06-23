47941026c5f0757e379cfdade9daeab2929d26df
rishabhdewang <rishabhdewang@gmail.com>
2021-06-23 11:34:12 +0530

    route added

--- app.ts
+++ app.ts
@@ -6,6 +6,10 @@ app.get('/', (req,res) => {
     res.send("Hello  World")
 })
 
+app.get('/api/heath-check', (req,res) => {
+    res.status(200).send("ok");
+})
+
 app.listen(3000, () => {
     console.log("server is Running on PORT: 3000")
 })
